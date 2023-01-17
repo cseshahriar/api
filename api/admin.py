ba609c60ea7d716afc32090dc98e342ac183a882
cseshahriar <cse.shahriar.hosen@gmail.com>
2023-01-17 08:29:16 +0600

    student model admin created

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Student
 
-# Register your models here.
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'roll', 'city', )
+    list_display_links = ('name', )
+    search_fields = ('name', )
+    ordering = ('-id', )
