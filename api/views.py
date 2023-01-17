9d79cf180f86c17237cb2b809fba805de4c80359
cseshahriar <cse.shahriar.hosen@gmail.com>
2023-01-17 08:49:14 +0600

    nameing convension update

--- api/views.py
+++ api/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.renderers import JSONRenderer
+from django.http import HttpResponse
 
-# Create your views here.
+from .serializers import StudentSerializer
+from .models import Student
+
+
+# Model Object - Single Student data
+def student_detail(request, pk):
+    student = Student.objects.get(pk=pk)
+    serializer = StudentSerializer(student, many=False)
+    json_data = JSONRenderer().render(serializer.data)
+    return HttpResponse(json_data, content_type='application/json')
