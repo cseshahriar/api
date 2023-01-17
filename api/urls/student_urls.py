9ef2b7fd5a7005be1135e79e0fbabab27c92fac4
cseshahriar <cse.shahriar.hosen@gmail.com>
2023-01-17 09:08:25 +0600

    student list api

--- api/urls/student_urls.py
+++ api/urls/student_urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from api.views import student_detail
+from api.views import student_detail, student_list
 
 urlpatterns = [
+    path('', student_list, name='student_list'),
     path('<int:pk>/', student_detail, name='student_detail'),
 ]
--- api/views.py
+++ api/views.py
@@ -7,7 +7,16 @@ from .models import Student
 
 # Model Object - Single Student data
 def student_detail(request, pk):
+    """ detail api using JSONRenderer """
     student = Student.objects.get(pk=pk)
     serializer = StudentSerializer(student, many=False)
+    json_data = JSONRenderer().render(serializer.data)
+    return HttpResponse(json_data, content_type='application/json')
+
+
+def student_list(request):
+    """ list api using JSONRenderer """
+    students = Student.objects.all()
+    serializer = StudentSerializer(students, many=True)
     json_data = JSONRenderer().render(serializer.data)
     return HttpResponse(json_data, content_type='application/json')
