89c541f5964a9b58a238c6c70b5a77f5170701e8
cseshahriar <cse.shahriar.hosen@gmail.com>
2023-01-17 09:35:42 +0600

    json response

--- api/views.py
+++ api/views.py
@@ -1,5 +1,5 @@
 from rest_framework.renderers import JSONRenderer
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
 from .serializers import StudentSerializer
 from .models import Student
@@ -10,13 +10,15 @@ def student_detail(request, pk):
     """ detail api using JSONRenderer """
     student = Student.objects.get(pk=pk)
     serializer = StudentSerializer(student, many=False)
-    json_data = JSONRenderer().render(serializer.data)
-    return HttpResponse(json_data, content_type='application/json')
+    # json_data = JSONRenderer().render(serializer.data)
+    # return HttpResponse(json_data, content_type='application/json')
+    return JsonResponse(serializer.data)
 
 
 def student_list(request):
     """ list api using JSONRenderer """
     students = Student.objects.all()
     serializer = StudentSerializer(students, many=True)
-    json_data = JSONRenderer().render(serializer.data)
-    return HttpResponse(json_data, content_type='application/json')
+    # json_data = JSONRenderer().render(serializer.data)
+    # return HttpResponse(json_data, content_type='application/json')
+    return JsonResponse(serializer.data, safe=False)
